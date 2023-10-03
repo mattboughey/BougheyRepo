c6dbed9ffbb4776cfeb929e74b6f4317df35dd79
mboughey <matthewrboughey@gmail.com>
2023-10-02 18:21:58 -0600

    Added a bear

--- C11A-1/src/MyDrawing.java
+++ C11A-1/src/MyDrawing.java
@@ -7,17 +7,20 @@ import javax.swing.JPanel;
 public class MyDrawing extends JPanel {
 	private Cat cat;
 	private Mouse mouse;
+	private Bear bear;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
 		cat = new Cat();
 		mouse = new Mouse();
+		bear = new Bear();
 	}
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
 		mouse.draw(g, 100, 200);
+		bear.draw(g,  250, 250);
 	}
 	
 	/**
