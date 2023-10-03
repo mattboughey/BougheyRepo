879c49389345cc137422d81c1d2aff6912781e7b
mboughey <matthewrboughey@gmail.com>
2023-10-02 19:51:08 -0600

    Re to first iteratoion

--- C12A-2/src/TheMotivator.java
+++ C12A-2/src/TheMotivator.java
@@ -1,31 +1,14 @@
 
 
 
-
 public class TheMotivator {
-	
-	
-	
-	
-	public void other() {
-		System.out.println("This should throw some errors hopefully");
-		feedback(35);
-		
-	}
-	
-	
-	
-	
 	public void feedback(int score) {
 		if (score == 100)
-			System.out.println("Conflict");
-		
+			System.out.println("You're awesome");
 		else if (score > 90)
 			System.out.println("That's great");
-		else if (score > 60) {
+		else if (score > 60)
 			System.out.println("That's good ");
-			System.out.println("This should hopefully bring an error??");
-	}
 		else
 			System.out.println("Well, what can I say?");
 	}
