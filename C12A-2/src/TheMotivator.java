1a74d875a141a7af9cc1876e1e9c5a55ed5816cc
mboughey <matthewrboughey@gmail.com>
2023-10-02 19:46:29 -0600

    first issue

--- C12A-2/src/TheMotivator.java
+++ C12A-2/src/TheMotivator.java
@@ -6,10 +6,13 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
+		
 		else if (score > 90)
 			System.out.println("That's great");
-		else if (score > 60)
+		else if (score > 60) {
 			System.out.println("That's good ");
+			System.out.println("This should hopefully bring an error??");
+	}
 		else
 			System.out.println("Well, what can I say?");
 	}
